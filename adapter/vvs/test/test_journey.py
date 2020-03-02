e4caccc3ae4a3ea0ad2b74eccfa25853489aab22
Marius Hegele <marius.hegele@ibm.com>
2020-03-02 17:34:07 +0100

    Small fixes

--- adapter/vvs/test/test_journey.py
+++ adapter/vvs/test/test_journey.py
@@ -6,14 +6,14 @@ from .. import Journey
 
 class TestJourney(unittest.TestCase):
     dirname = os.path.dirname(__file__)
-    with open(os.path.join(dirname, 'mock_journey.json'), 'r') as mock_journey_f:
-        mock_journeys = json.load(mock_journey_f).get('journeys')
+    with open(os.path.join(dirname, 'mock_journeys.json'), 'r') as mock_journeys_f:
+        mock_journeys = json.load(mock_journeys_f).get('journeys')
 
     def test_from_vvs(self):
         journeys = []
         for mock_json in self.mock_journeys:
            journeys.append(Journey().from_vvs(mock_json)) 
-        self.assertTrue(len(journeys) == 5)
+        self.assertTrue(len(journeys) == 3)
 
         for journey in journeys:
             self.assertIsNotNone(journey)
@@ -22,5 +22,4 @@ class TestJourney(unittest.TestCase):
         journey = journeys[0]
         self.assertEqual(journey.origin, 'Stadtmitte')
         self.assertEqual(journey.dest, 'Stuttgart Hauptbahnhof (tief)')
-        from nose.tools import set_trace; set_trace()
         self.assertEqual(len(journey.legs), 1)
