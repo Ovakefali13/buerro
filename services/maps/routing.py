f7ca6a0a3034a1629d72aefc562906bd83cbee4e
jannistriestocode <jannis@rauschke.eu>
2020-03-03 21:06:22 +0100

    Change logic of map service

--- services/maps/routing.py
+++ services/maps/routing.py
@@ -2,7 +2,6 @@ import requests
 from openrouteservice import client
 from geocoding import getCoordsFromAddress
 
-
 class RouteRequest:
     # openroute service
     API_TOKEN = '5b3ce3597851110001cf62488ef50484ef524d228826ecc7b35f5df1'
@@ -10,38 +9,44 @@ class RouteRequest:
     def __init__(self):
         print('Init Map Service')
 
-        clnt = client.Client(key=self.API_TOKEN)
+        self.clnt = client.Client(key=self.API_TOKEN)
+        self.route = None
 
         self.PREFS = {
-            "profile": "driving-car",
-            "preference": "shortest"
+            'profile': 'driving-car',
+            'preference': 'shortest'
         }
 
         self.request_params = {
             'coordinates': [[12.108259, 54.081919], [12.072063, 54.103684]],
             'format_out': 'json',
-            'profile': self.PREFS["profile"],
-            'preference': self.PREFS["preference"],
+            'profile': self.PREFS['profile'],
+            'preference': self.PREFS['preference'],
             'instructions': 'false',
             'geometry': 'false',            
         }
 
-        def setRoute(self, start, dest):
-            if isinstance(start, list) and isinstance(dest, list):
-                self.request_params['coordinates'] = [start, dest]
-            elif isinstance (start, str) and isinstance(dest, str):
-                self.request_params['coordinates'] = [getCoordsFromAddress(start), getCoordsFromAddress(dest)]
-                
-        def setTravelMode(self, profile):
-            self.request_params['profile'] = profile
+    def __set_route(self, start, dest):
+        if isinstance(start, list) and isinstance(dest, list):
+            self.request_params['coordinates'] = [start, dest]
+        elif isinstance (start, str) and isinstance(dest, str):
+            self.request_params['coordinates'] = [getCoordsFromAddress(start), getCoordsFromAddress(dest)]
+            
+    def __load_route_information(self, start, dest):
+        self.__set_route(start, dest)
+        self.route = self.clnt.directions(**self.request_params)   
 
-        def getRouteInformation(self):
-            self.route = clnt.directions(**self.request_params)     
+    def __set_travel_mode(self, profile):
+        self.request_params['profile'] = profile
 
-        def printRouteSummary(self):
-            distance = self.route['routes'][0]['summary']['distance']
-            duration = self.route['routes'][0]['summary']['duration'] 
-            print(f"The route is {round(distance / 1000)} km long and takes {round(duration / 60)} minutes.")
+    def get_route_summary(self, start, dest, travel_mode = None):
+        if travel_mode:
+            self.__set_travel_mode(travel_mode)      
+        self.__load_route_information(start, dest)
+        return (self.route['routes'][0]['summary']['distance'], self.route['routes'][0]['summary']['duration'] )
+        
 
-        getRouteInformation(self)
-        printRouteSummary(self)
+
+router = RouteRequest()
+print(router.get_route_summary('Rotebühlplatz 41-1, 70178, Stuttgart','Holzgartenstraße 11, 70174 Stuttgart'))
+print(router.get_route_summary([9.170963, 48.773563],[9.169989, 48.780834]))
