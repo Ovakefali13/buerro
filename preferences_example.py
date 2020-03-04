17c479bddd9b020e7de8900cd53745299b32b680
Ovakefali13 <Ovakefali13@web.de>
2020-03-04 17:40:08 +0100

    Removed example

--- preferences_example.py
+++
@@ -1,9 +0,0 @@
-import services.preferences.pref_service as pa
-import services.preferences.test.test_service as pa_test
-
-test = pa.PrefService(pa.PrefJSONRemote())
-print(test.get_preferences("cooking"))
-
-unitTest = pa_test.TestPrefService()
-print(unitTest.test_get_apiKey)
-print(unitTest.test_merge_Json_Files)
