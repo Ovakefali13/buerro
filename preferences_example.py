8e30faa06c9e78dca18fe46c81657312ebc30325
Ovakefali13 <Ovakefali13@web.de>
2020-03-04 12:22:07 +0100

    added abstract preferences, changed method names and created instances

--- preferences_example.py
+++ preferences_example.py
@@ -1,4 +1,4 @@
 import services.preferences.preferences_adapter as pa
 
-test = pa.PrefService(pa.PrefJSONRemote)
-print(test.getPreferences("cooking"))
+test = pa.PrefService(pa.PrefJSONRemote())
+print(test.get_preferences("cooking"))
--- services/preferences/preferences_adapter.py
+++ services/preferences/preferences_adapter.py
@@ -1,11 +1,21 @@
 import json
 import os
+from abc import ABC, abstractmethod
 
-class PrefJSONRemote():
-    def loadFile():
+class PrefRemote(ABC):
+    @abstractmethod
+    def load_File(self):
+        pass
+
+    @abstractmethod
+    def merge_Json_Files(self, dict1:dict, dict2:dict):
+        pass
+
+class PrefJSONRemote(PrefRemote):
+    def load_File(self):
         preferences_json = json.load(open('preferences.json'))
         return preferences_json
-    def mergerJsonFiles(dict1, dict2): 
+    def merge_Json_Files(self, dict1, dict2): 
         res = {**dict1, **dict2} 
         return res
 
@@ -15,7 +25,7 @@ class PrefService:
     def __init__(self, remote):
         self.remote = remote
 
-    def getPreferences(self, key):
-        preferences_json = self.remote.loadFile()
-        return_json = self.remote.mergerJsonFiles(preferences_json["general"], preferences_json[key])
+    def get_preferences(self, key):
+        preferences_json = self.remote.load_File()
+        return_json = self.remote.merge_Json_Files(preferences_json["general"], preferences_json[key])
         return return_json
