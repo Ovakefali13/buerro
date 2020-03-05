3de88cb98fa732f062d99a10de1094842c5fac1c
Marius Hegele <marius.hegele@ibm.com>
2020-03-05 06:18:20 +0100

    Fix wrong usages of pref service, camel_Underscore to under_score

--- services/preferences/pref_service.py
+++ services/preferences/pref_service.py
@@ -4,28 +4,28 @@ from abc import ABC, abstractmethod
 
 class PrefRemote(ABC):
     @abstractmethod
-    def load_File(self):
+    def load_file(self):
         pass
 
     @abstractmethod
-    def merge_Json_Files(self, dict1:dict, dict2:dict):
+    def merge_json_files(self, dict1:dict, dict2:dict):
         pass
 
 class PrefJSONRemote(PrefRemote):
-    def load_File(self):
+    def load_file(self):
         preferences_json = json.load(open('preferences.json'))
         return preferences_json
-    def merge_Json_Files(self, dict1, dict2): 
-        res = {**dict1, **dict2} 
+    def merge_json_files(self, dict1, dict2):
+        res = {**dict1, **dict2}
         return res
 
-class PrefService: 
+class PrefService:
     remote = None
 
-    def __init__(self, remote):
+    def __init__(self, remote=PrefJSONRemote()):
         self.remote = remote
 
     def get_preferences(self, key):
-        preferences_json = self.remote.load_File()
-        return_json = self.remote.merge_Json_Files(preferences_json["general"], preferences_json[key])
+        preferences_json = self.remote.load_file()
+        return_json = self.remote.merge_json_files(preferences_json["general"], preferences_json[key])
         return return_json
