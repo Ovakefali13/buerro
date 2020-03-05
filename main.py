05552a558febf9ece8eda2cc43871adc290bd2cd
Ovakefali13 <Ovakefali13@web.de>
2020-03-05 15:31:22 +0100

    changed spoonacular service to atom methods + changed unit tests (75% coverage)

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from services.weatherAPI.weather_service import WeatherAdapter
-from usecase import lunch
+#from services.weatherAPI.weather_service import WeatherAdapter
+from services.spoonacular.spoonacular_service import SpoonacularService, SpoonacularJSONRemote
+#from usecase import lunch
 import json
 def main():
-    weatherAdapter = WeatherAdapter.instance()
-    weatherAdapter.update('Stuttgart')
-    print(weatherAdapter.isBadWeather())
-    print(weatherAdapter.willBeBadWeather(3))
-    print(weatherAdapter.willBeBadWeather(6))
-    print(weatherAdapter.willBeBadWeather(9))
-    print(weatherAdapter.weatherForecast)
-    print(weatherAdapter.weather)
+    spoonacular_service = SpoonacularService(SpoonacularJSONRemote())
+    spoonacular_service.get_recipe_reduced_by_ingredient('pork')
+    #weatherAdapter = WeatherAdapter.instance()
+    #weatherAdapter.update('Stuttgart')
+    #print(weatherAdapter.isBadWeather())
+    #print(weatherAdapter.willBeBadWeather(3))
+    #print(weatherAdapter.willBeBadWeather(6))
+    #print(weatherAdapter.willBeBadWeather(9))
+    #print(weatherAdapter.weatherForecast)
+    #print(weatherAdapter.weather)
 
 
 if __name__ == '__main__':
