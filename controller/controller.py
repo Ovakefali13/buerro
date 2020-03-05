36fa3901281373646d8f57599fbf3b97e5690746
Marius Hegele <marius.hegele@ibm.com>
2020-03-05 09:22:19 +0100

    Handle basic Post Request

--- controller/controller.py
+++ controller/controller.py
@@ -1,51 +1,40 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib
 
-hostName = "localhost"
-serverPort = 8080
 
-def Controller(BaseHTTPRequestHandler):
+class Controller(BaseHTTPRequestHandler):
+    """
+    headers = {
+        'Content-type', 'application/json'
+    }
+    """
 
     def do_POST(self):
-        # Parse the form data posted
-        form = cgi.FieldStorage(
-            fp=self.rfile,
-            headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
+        def _set_headers():
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-        # Begin the response
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write('Client: %s\n' % str(self.client_address))
-        self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
-        self.wfile.write('Path: %s\n' % self.path)
-        self.wfile.write('Form data:\n')
+        def respond(msg:str):
+            self.wfile.write(msg.encode("utf-8"))
 
-        # Echo back information about what was posted in the form
-        for field in form.keys():
-            field_item = form[field]
-            if field_item.filename:
-                # The field contains an uploaded file
-                file_data = field_item.file.read()
-                file_len = len(file_data)
-                del file_data
-                self.wfile.write('\tUploaded %s as "%s" (%d bytes)\n' % \
-                        (field, field_item.filename, file_len))
-            else:
-                # Regular form value
-                self.wfile.write('\t%s=%s\n' % (field, form[field].value))
+        def _shutdown():
+            _set_headers()
+            respond('shutdown')
+
+        length = int(self.headers['Content-Length'])
+        body = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
+        msg = body.get('message', '')[0]
+        if msg == 'ping':
+            _set_headers()
+            respond('pong')
+        elif msg == 'shutdown':
+            _shutdown()
+        else:
+            self.send_response(400)
+            self.end_headers()
+        del msg
 
 
-if __name__ == "__main__":
-    controller = HTTPServer((hostName, serverPort), Controller)
-    print("Server started http://%s:%s" % (hostName, serverPort))
 
-    try:
-        controller.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    controller.server_close()
-    print("Server stopped.")
--- controller/test/test_controller.py
+++ controller/test/test_controller.py
@@ -1,25 +1,41 @@
 import requests
 import unittest
+import time
 
 from http.server import HTTPServer
-from socketserver import ThreadingMixIn
+from threading import Thread, Event
 
 from .. import Controller
-
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    """Handle requests in a separate thread"""
 
 class TestController(unittest.TestCase):
     @classmethod
     def setUp(self):
         self.hostName = "localhost"
-        self.serverPort = "8089"
+        self.serverPort = 8089
+        self.server_url = "http://" + self.hostName + ":" + str(self.serverPort)
+        self.controller = HTTPServer((self.hostName, self.serverPort), Controller)
 
-        self.controller = ThreadedHTTPServer((hostName, serverPort), Controller)
-        print("Server started http://%s:%s" % (hostName, serverPort))
-        self.controller.serve_forever()
+        self.ready_event = Event()
+        self.shutdown_event = Event()
+        def in_thread():
+            self.ready_event.set()
+            while not self.shutdown_event.is_set():
+                self.controller.handle_request()
+
+        self.server_thread = Thread(target=in_thread)
+        self.server_thread.start()
+
+        time.sleep(0.0001)
+        self.ready_event.wait(3)
+        if not self.ready_event.is_set():
+            raise Exception("most likely failed to start server")
 
     def test_post_message(self):
-        url = self.hostName + ":" + self.serverPort
-        resp = requests.post(url, data="ping")
-        self.assertEqual(resp, "pong")
+        resp = requests.post(self.server_url, data={"message": "ping"})
+        self.assertEqual(resp.content, b"pong")
+
+    @classmethod
+    def tearDown(self):
+        self.shutdown_event.set()
+        requests.post(self.server_url, data={'message':'shutdown'})
+        self.server_thread.join(0.1)
