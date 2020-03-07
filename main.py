d716b3beefc0984c96ad6ecb5239131d495c5ad1
Timo Zuther <zutimo15@gmail.com>
2020-03-07 14:46:41 +0100

    cleaned up main py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from services.weatherAPI.weather_service import WeatherAdapter
 from usecase import lunch
 import json
 def main():
-    weatherAdapter = WeatherAdapter.instance()
-    weatherAdapter.update('Stuttgart')
-    print(weatherAdapter.is_bad_weather())
-    print(weatherAdapter.will_be_bad_weather(3))
-    print(weatherAdapter.will_be_bad_weather(6))
-    print(weatherAdapter.will_be_bad_weather(9))
-    print(weatherAdapter.weatherForecast)
-    print(weatherAdapter.weather)
-
     l = lunch.Lunchbreak('Stuttgart')
 
 
