dc20e6d4431d08afd30d51bf30a2e3aa008833e5
Timo Zuther <zutimo15@gmail.com>
2020-03-13 10:18:11 +0100

    setup method for usecase test

--- usecase/lunchbreak/test/test_usecase.py
+++ usecase/lunchbreak/test/test_usecase.py
@@ -7,45 +7,45 @@ from services.cal.event import Event
 
 class TestLunchbreak(unittest.TestCase):
 
-    if 'DONOTMOCK' in os.environ:
-        MOCK = False
-    else:
-        print("Mocking remotes...")
-        MOCK = True
-    MOCK_LOCATION = [48.76533759999999, 9.161932799999999]
-    dhbw = [48.773563, 9.170963]
+    @classmethod
+    def setUpClass(self):
+        if 'DONOTMOCK' in os.environ:
+            self.MOCK = False
+        else:
+            print("Mocking remotes...")
+            self.MOCK = True
+        self.MOCK_LOCATION = [48.76533759999999, 9.161932799999999]
+        self.dhbw = [48.773563, 9.170963]
+
+    @classmethod
+    def setUp(self):
+        self.lb = Lunchbreak(self.MOCK)
 
     def test_check_lunch_options(self):
-        lb = Lunchbreak(self.MOCK)
-        nearby_restaurants, start, end = lb.check_lunch_options(self.dhbw)
+        nearby_restaurants, start, end = self.lb.check_lunch_options(self.dhbw)
         self.assertIsInstance(nearby_restaurants, list)
 
     def test_time_diff_in_hours(self):
-        lb = Lunchbreak(self.MOCK)
         start = '2020-05-05T12:00:00'
         start_iso = datetime.fromisoformat(start)
 
         end = '2020-05-06T12:00:00'
         end_iso = datetime.fromisoformat(end)
 
-        diff_hours = lb.time_diff_in_hours(end_iso, start_iso)
+        diff_hours = self.lb.time_diff_in_hours(end_iso, start_iso)
         self.assertEqual(diff_hours, 24)
 
     def test_open_maps_route(self):
-        lb = Lunchbreak(self.MOCK)
-        nearby_restaurants, start, end = lb.check_lunch_options(self.dhbw)
-
-        google_link = lb.open_maps_route(self.dhbw, nearby_restaurants[1])
+        nearby_restaurants, start, end = self.lb.check_lunch_options(self.dhbw)
+        google_link = self.lb.open_maps_route(self.dhbw, nearby_restaurants[1])
         self.assertIsInstance(google_link, str)
 
     def test_notify(self):
-        lb = Lunchbreak(self.MOCK)
-        is_active = lb.notify()
+        is_active = self.lb.notify()
         self.assertIsInstance(is_active, bool)
 
     def test_create_cal_event(self):
-        lb = Lunchbreak(self.MOCK)
-        nearby_restaurants, start, end = lb.check_lunch_options(self.dhbw)
-        google_link = lb.open_maps_route(self.dhbw, nearby_restaurants[1])
-        ret = lb.create_cal_event(start,end, nearby_restaurants[1], google_link)
+        nearby_restaurants, start, end = self.lb.check_lunch_options(self.dhbw)
+        google_link = self.lb.open_maps_route(self.dhbw, nearby_restaurants[1])
+        ret = self.lb.create_cal_event(start,end, nearby_restaurants[1], google_link)
         self.assertIsInstance(ret, Event)
