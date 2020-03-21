888a8f543787b709f4479e7f908dafce2d7a675d
jannistriestocode <jannis@rauschke.eu>
2020-03-21 20:55:43 +0100

    Add basic structure

--- usecase/transport/transport.py
+++ usecase/transport/transport.py
@@ -2,10 +2,15 @@ from services.weatherAPI.weather_service import WeatherAdapter
 from services.preferences import PrefService, PrefJSONRemote
 from services.maps import GeocodingService, MapService
 from services.vvs import VVSService, VVSEfaJSONRemote
-from multiprocessing import Process
-from datetime import datetime
+from usecase import Usecase, Reply
+from handler import LocationHandler
 
-class Transport: 
+from multiprocessing import Process
+from datetime import datetime, timedelta
+import re
+
+
+class Transport(Usecase): 
     transport_info = {
         'Start': None,
         'Dest': None,
@@ -19,37 +24,115 @@ class Transport:
     }
     
     def __init__(self):
-        self.trigger_use_case([48.773563, 9.170963],[48.780834, 9.169989],'dep')
-    
+        self.set_services()
+        self.advance('I want to go from home to the university and arrive at 10 pm.')
+
+
+    def set_services(self,
+                    pref_service:PrefService=PrefService(PrefJSONRemote()),
+                    map_service:MapService=MapService.instance(),
+                    vvs_service:VVSService=VVSService.instance(VVSEfaJSONRemote.instance()),
+                    geo_service:GeocodingService=GeocodingService.instance(),
+                    wea_service:WeatherAdapter=WeatherAdapter.instance()):
+        self.pref_service = pref_service
+        self.pref = pref_service.get_preferences('transport')
+        self.map_service = map_service
+        self.vvs_service = vvs_service
+        self.geo_service = geo_service
+        self.wea_service = wea_service
+
+
+    def advance(self, message):
+        if not self.wea_service:
+            raise Exception("Set Services!")
+
+        if not self.transport_info['Start']:
+            self.set_transport_parameters(message)
+
+        if not self.transport_info['Cycling']:
+            self.get_transport_information()
+
+        
+    def is_finished(self):
+        return self.is_finished()     
+
+    def set_transport_parameters(self, message):
+        special_locations = ['home', 'work', 'mainstation', 'university']
+
+        def get_special_location(location:str):
+            return self.pref[location + '_coords']
+
+        def get_datetime(time:str):
+            strings = time.split()
+            hours = int(strings[0])
+            now = datetime.now()
+            date = None
             
-    def trigger_use_case(self, start:list, dest:list, arr_dep:str, time:datetime=datetime.now()):
-        print('trigger_use_case')
+            if 'hours' in strings:
+                date = now + timedelta(hours=hours)
+            elif 'pm' in strings:
+                date = now.replace(hour=hours+12,minute=0,second=0)     
 
-        self.transport_info['Start'] = start
-        self.transport_info['Dest'] = dest
-        self.transport_info['ArrDep'] = arr_dep
-        self.transport_info['Time'] = time
+            return date        
 
-        self.load_preferences()
-        self.get_transport_information()
-    
+        def get_location():
+            lh = LocationHandler.instance()
+            return lh.get()
 
-    def load_preferences(self):
-        print('load_preferences')
-        self.pref_service = PrefService(PrefJSONRemote())
-        self.preferences_json = self.pref_service.get_preferences('transport')   
+        """ Regex tested with
+        I want to go from Stuttgart to Abstatt and arrive at 4 pm.
+        Now I want to go to Abstatt.
+        I want to go from home to the university and arrive at 10 pm.
+        I want to go home in 2 hours.
+        I want to go home now.
+        I want to go home at 6 pm. 
+        I want to go to Stuttgart and depart at 7 am.
+        I want to go from the mainstation to home.
+        """
+
+        if not self.transport_info['Start']:      
+            regex = r'((?<=from\sthe\s)|(?<=from\s(?!the)))(\w*|home)'
+            start = re.search(regex, message)[0]
+
+            if start:
+                if start in special_locations:
+                    self.transport_info['Start'] = get_special_location(start)
+                else:
+                    self.transport_info['Start'] = self.geo_service.get_coords_from_address(start)
+            else:
+                self.transport_info['Start'] = get_location()
+
+        if not self.transport_info['Dest']:
+            p = re.compile(r'((?<=to\sthe\s)|(?<=to\s(?!(the|go|arrive))))(\w*|home)|(?<=go\s)home')
+            dest = p.search(message)[0]
+            if dest:
+                if dest in special_locations:
+                    self.transport_info['Dest'] = get_special_location(dest)
+                else:
+                    self.transport_info['Dest'] = self.geo_service.get_coords_from_address(dest)
+        
+        if not self.transport_info['ArrDep']:
+            if 'arrive at' in message:
+                self.transport_info['ArrDep'] = 'Arr'
+            else:
+                self.transport_info['ArrDep'] = 'Dep'
+            
+        if not self.transport_info['Time']:
+            p = re.compile(r'(?<=at\s)\d{1,2}\s(am|pm)|\d{1,2}\shours')
+            time = p.search(message)[0]
+            if time:
+                self.transport_info['Time'] = get_datetime(time)
+            else:
+                self.transport_info['Time'] = datetime.now()
+
 
 
     def get_transport_information(self):
-        print('get_transport_options')        
-        geo = GeocodingService.instance()    
-
+        print('get_transport_information')
         
-        def weather_service(self):
-            wea = WeatherAdapter.instance()            
-            wea.update(geo.get_city_from_coords(self.transport_info['Start']))
-            
-            self.transport_info['WeatherGood'] = wea.is_bad_weather()
+        def weather_service(self):   
+            self.wea_service.update(self.geo_service.get_city_from_coords(self.transport_info['Start']))            
+            self.transport_info['WeatherGood'] = self.wea_service.is_bad_weather()
 
 
         def map_service(self):
@@ -58,23 +141,17 @@ class Transport:
                 'Cycling': 'cycling-regular',
                 'Walking': 'foot-walking'
             }
-
-            maps = MapService.instance()            
-
             for name, mode in args.items():
-                print(name, mode)
-
-                self.transport_info[name] = maps.get_route_summary(self.transport_info['Start'], self.transport_info['Dest'], mode)
+                self.transport_info[name] = self.map_service.get_route_summary(self.transport_info['Start'], self.transport_info['Dest'], mode)
 
 
         def vvs_service(self):
-            vvs = VVSService(VVSEfaJSONRemote())
-            start = geo.get_address_from_coords(self.transport_info['Start'])
-            dest = geo.get_address_from_coords(self.transport_info['Dest'])
+            start = self.geo_service.get_address_from_coords(self.transport_info['Start'])
+            dest = self.geo_service.get_address_from_coords(self.transport_info['Dest'])
             arr_dep = self.transport_info['ArrDep']
-            time = self.transport_info['Time']
-            
-            self.transport_info['VVS'] = vvs.get_journeys(start, dest, arr_dep, time)
+            time = self.transport_info['Time']           
+            print(start, dest, arr_dep, time) 
+            self.transport_info['VVS'] = self.vvs_service.get_journeys(start, dest, arr_dep, time)
             
 
         def runInParallel(*fns):
