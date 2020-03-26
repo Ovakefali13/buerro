11facaabe1d9ce53720c3ecf45b790b47b5d3006
Marius Hegele <marius.hegele@ibm.com>
2020-03-26 18:55:20 +0100

    Fix path for service worker

--- frontend/www/js/main.js
+++ frontend/www/js/main.js
@@ -35,7 +35,7 @@ function subscribeUser() {
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
 
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/js/service-worker.js')
         .then((registration) => {
             console.log('ServiceWorker registration successful: ', registration);
             swRegistration = registration
