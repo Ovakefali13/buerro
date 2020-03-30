b88b39c1e3f8431cd0b942eb9dd2123985874a8f
Marius Hegele <marius.hegele@ibm.com>
2020-03-30 06:52:57 +0200

    Saved enabled/disabled option in local storage

--- frontend/www/js/tts.js
+++ frontend/www/js/tts.js
@@ -1,14 +1,23 @@
 var running = false;
-var enabled = true;
 var synth = window.speechSynthesis;
+
+enabled = !(window.localStorage.getItem('ttsEnabled') == 'false')
+
+$(document).ready(() => {
+    if (!enabled) {
+        $("#speak").css("background-color","Gray");
+    }
+});
 
 function toggle_speech() {
     if(enabled) {
         synth.cancel()
         enabled = false;
+        window.localStorage.setItem('ttsEnabled', false);
         $("#speak").css("background-color","Gray");
     } else {
         enabled = true;
+        window.localStorage.setItem('ttsEnabled', true);
         $("#speak").css("background-color","Green");  
     }
 }
